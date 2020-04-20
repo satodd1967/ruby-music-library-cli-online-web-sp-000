ead9425d48d38392f43ba1377c352480c64e9c87
Seth Todd <seth.todd@outlook.com>
2020-04-20 02:06:54 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -77,7 +77,6 @@ class MusicLibraryController
       Song.all.sort_by! {|songs| songs.name}
       playing = Song.all.each.with_index {|songs, index| index == input2}
       playing.each.with_index {|song| puts "Playing #{song.name} by #{song.artist.name}"}
-    end
   end
   
   
